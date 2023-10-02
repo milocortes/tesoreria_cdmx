db25933fe5c76fabac2ac4c9a50e34f3ce75ad0c
Mateo92 <mateotonatiuh@ciencias.unam.mx>
2023-10-02 14:56:37 -0600

    Participacion_por_sector_updated

---
+++ IMSS_NOMINA/src/crosswalk_tesoreria_imss.py
@@ -0,0 +1,143 @@
+## V15
+tesoreria_v15 = {   1: 'Comercio al por Menor',
+                    2: 'Comercio al por Mayor',
+                    3: 'NA',
+                    4: 'Servicios Profesionales, Técnicos, Especializados y Personales',
+                    5: 'Servicios de Alquiler y Administración de Bienes inmueble',
+                    6: 'Servicios Educativos, Investigación, Médicos y de Asistencia social y de Asociaciones Civil y Religiosas',
+                    7: 'Servicios Financieros, de Seguros y Fianzas',
+                    8: 'Construcción',
+                    9: 'Servicios de Reparación y Mantenimiento',
+                    10: 'Servicios de Comunicaciones',
+                    11: 'Servicios de Administración Pública, Defensa y Saneamiento',
+                    12: 'Textiles, Prendas de Vestir e Industria de Cuero',
+                    13: 'Transportes',
+                    14: 'Restaurantes',
+                    15: 'Servicios de Esparcimiento, Culturales, Recreativos y Deportivos',
+                    16: 'Hoteles',
+                    17: 'Papel y Productos de Papel, Imprentas y Editoriales',
+                    18: 'Servicios de Alquiler y Administración de Bienes Inmueble',
+                    19: 'Productos Alimenticios, Bebidas y Tabacos',
+                    20: 'Otras Industrias Manufactureras',
+                    21: 'Servicios Relacionados con la Agricultura, Ganadería, Construcción, Transportes, Financieros y Comercio',
+                    22: 'Sustancias Químicas, Productos derivados del Petroleo, carbón, hule y plástico',
+                    23: 'Industria de la Madera y Productos de la Madera',
+                    24: 'Electricidad',
+                    25: 'Captación, Potabilización y Distribución de Agua',
+                    26: 'Agricultura, Ganadería y Caza',
+                    27: 'Industrias Metálicas Básicas',
+                    28: 'Productos Metálicos, Maquinaria y Equipo',
+                    29: 'Carbón',
+                    30: 'Petroleo y Gas Natural',
+                    31: 'Pesca',
+                    32: 'Productos Minerales no Metálicos, Excepto derivados del petroleo',
+                    33: 'Extracción de Materiales Metálicos',
+                    34: 'Extracción de Minerales no Metálicos',
+                    35: 'Servicios de Organismos Internacionales',
+                    36: 'Silvicultura'
+                    }
+
+
+## IMSS sector_economico_2_2pos ---- descripcion_sector_economico_2
+
+cw_sec_2_2pos_descrip = {"1" : "Agricultura",
+                        "2" : "Ganadería",
+                        "3" : "Silvicultura",
+                        "4" : "Pesca",
+                        "5" : "Caza",
+                        "11" : "Extracción  y  beneficio de carbón mineral, grafito y minerales no metálicos; excepto sal",
+                        "12" : "Exploración y extracción  de  petróleo crudo y gas natural (no incluye a Pemex)",
+                        "13" : "Extracción y beneficio de minerales metálicos",
+                        "14" : "Explotación de sal",
+                        "20" : "Fabricación de alimentos",
+                        "21" : "Elaboración de bebidas",
+                        "22" : "Beneficio y fabricación de productos de tabaco",
+                        "23" : "Industria textil",
+                        "24" : "Confección de prendas de vestir y otros artículos confeccionados con textiles y otros materiales; excepto calzado",
+                        "25" : "Fabricación de  calzado e industria del cuero",
+                        "26" : "Industria y productos de madera y corcho; excepto muebles",
+                        "27" : "Fabricación y reparación de muebles de madera y sus partes; excepto los de metal y plástico moldeado",
+                        "28" : "Industria del papel",
+                        "29" : "Industrias editorial, de impresión y conexas",
+                        "30" : "Industria química",
+                        "31" : "Refinación del petróleo y derivados del carbón mineral (no incluye Pemex)",
+                        "32" : "Fabricación de productos de hule y plástico",
+                        "33" : "Fabricación de productos de minerales no metálicos;  excepto del petróleo y del carbón mineral",
+                        "34" : "Industrias metálicas básicas",
+                        "35" : "Fabricación de productos metálicos; excepto maquinaria y equipo",
+                        "36" : "Fabricación, ensamble y reparación de maquinaria, equipo y sus partes; excepto los eléctricos",
+                        "37" : "Fabricación y ensamble de maquinaria, equipos, aparatos, accesorios y artículos eléctricos, electrónicos y sus partes",
+                        "38" : "Construcción, reconstrucción y ensamble de equipo de transporte y sus partes",
+                        "39" : "Otras industrias manufactureras",                    
+                        "41" : "Construcción de edificaciones y de obras de ingeniería civil",
+                        "42" : "Trabajos realizados por contratistas especializados",                                                 
+                        "50" : "Generación, transmisión y distribución de energía eléctrica",                                                                 
+                        "51" : "Captación y suministro de agua potable",                                             
+                        "61" : "Compraventa de alimentos, bebidas y productos del tabaco",                                                                         
+                        "62" : "Compraventa de prendas de vestir y otros artículos de uso personal",
+                        "63" : "Compraventa de artículos para el hogar",                      
+                        "64" : "Compraventa en tiendas de autoservicio y de departamentos especializados por línea de mercancías",
+                        "65" : "Compraventa de gases, combustibles y lubricantes",
+                        "66" : "Compraventa de materias primas, materiales y auxiliares",                                               
+                        "67" : "Compraventa de maquinaria, equipo, instrumentos, aparatos, herramientas;  sus refacciones y accesorios",
+                        "68" : "Compraventa de equipo de transporte; sus refacciones y accesorios",
+                        "69" : "Compraventa de inmuebles y artículos diversos",
+                        "71" : "Transporte terrestre",                                      
+                        "72" : "Transporte por agua",                                                                                                                      
+                        "73" : "Transporte aéreo",                                                                          
+                        "74" : "Servicios conexos al transporte",
+                        "75" : "Servicios relacionados con el transporte en general",
+                        "76" : "Comunicaciones",                                                                                       
+                        "81" : "Servicios financieros y de seguros (bancos, financieras, compañías de seguros, etc.)",
+                        "82" : "Servicios colaterales a las instituciones financieras y de seguros",                                         
+                        "83" : "Servicios relacionados con inmuebles",    
+                        "84" : "Servicios profesionales y técnicos",                                                                                                        
+                        "85" : "Servicios de alquiler; excepto de inmuebles",                                                                                                    
+                        "86" : "Servicios de alojamiento temporal",                                                                                      
+                        "87" : "Preparación y servicio de alimentos y bebidas",                                                                                       
+                        "88" : "Servicios recreativos y de esparcimiento",                                                                                
+                        "89" : "Servicios personales para el hogar y diversos",
+                        "91" : "Servicios de enseñanza, investigación científica y difusión cultural",
+                        "92" : "Servicios médicos, asistencia social y veterinarios",                                                                                 
+                        "93" : "Agrupaciones mercantiles, profesionales, cívicas, políticas, laborales y religiosas",                                                 
+                        "94" : "Servicios de administración pública y seguridad social",                                          
+                        "99" : "Servicios de organizaciones internacionales y otros organismos extraterritoriales"}
+
+# Crosswalk: Tesorería-IMSS
+
+cw_tesoreria_imss={
+    '1_2': ['61', '62', '63','64', '65', '66','67' ,'68','69'], # 'Comercio al por Mayor' y 'Comercio al por Menor'
+ '4': ['84'], # 'Servicios Profesionales, Técnicos, Especializados y Personales'
+ '5': ['83','85'], # 'Servicios de Alquiler y Administración de Bienes inmueble',
+ '6': ['91', '92', '93'],# 'Servicios Educativos, Investigación, Médicos y de Asistencia social y de Asociaciones Civil y Religiosas'
+ '7': ['81', '82'], #'Servicios Financieros, de Seguros y Fianzas'
+ '8': ['41'], #'Construcción'
+ '9':["38"],#'Servicios de Reparación y Mantenimiento'
+ '10':[ "76"] ,# 'Servicios de Comunicaciones'
+ '11': ['94'], # 'Servicios de Administración Pública, Defensa y Saneamiento'
+ '12': ['23', '24',"25"], #'Textiles, Prendas de Vestir e Industria de Cuero'
+ '13': ['71', '72', '73',"74","75"], # 'Transportes'
+ '14': ['87'], # 'Restaurantes'
+ '15': ['88'], # Servicios de Esparcimiento, Culturales, Recreativos y Deportivos'
+ '16': ['86'], # 'Hoteles'
+ '17': ['28','29'], # 'Papel y Productos de Papel, Imprentas y Editoriales'
+ '18': ['83','85'], # 'Servicios de Alquiler y Administración de Bienes Inmueble'
+ '19': ['20', '21', '22'], #'Productos Alimenticios, Bebidas y Tabacos'
+ '20': ['39'], # 'Otras Industrias Manufactureras'
+ '21': ['74','75','82',"83"], # 'Servicios Relacionados con la Agricultura, Ganadería, Construcción, Transportes, Financieros y Comercio'
+ '22': ['30','31','32'], # 'Sustancias Químicas, Productos derivados del Petroleo, carbón, hule y plástico'
+ '23': ['26',"27"], # 'Industria de la Madera y Productos de la Madera'
+ '24': ['50'], # 'Electricidad'
+ '25': ['51'], # 'Captación, Potabilización y Distribución de Agua'
+ '26': ['1', '2','5'], # 'Agricultura, Ganadería y Caza'
+ '27': ['34'], # 'Industrias Metálicas Básicas'
+ '28': ["35",'36'], # 'Productos Metálicos, Maquinaria y Equipo'
+ '29': ["11" ,'31'], # 'Carbón'
+ '30': ['12',"31" ], # 'Petroleo y Gas Natural'
+ '31': ['5'], # 'Pesca'
+ '32': ['33'], # 'Productos Minerales no Metálicos, Excepto derivados del petroleo'
+ '33': ['13'],# 'Extracción de Materiales Metálicos'
+ '34': ['11'],# 'Extracción de Minerales no Metálicos'
+ '35': ['99'], # 'Servicios de Organismos Internacionales'
+ '36': ['3'] # 'Silvicultura'
+}
